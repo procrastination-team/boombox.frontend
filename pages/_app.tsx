ae1d7ef5b001373c37163a90a7ceb35e75aa0f8d
geekan <geekan@bk.ru>
2021-11-06 19:50:10 +0500

    Setting up the main environment

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import React from 'react';
+
+/**
+ * @param appProps - main application props
+ */
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
