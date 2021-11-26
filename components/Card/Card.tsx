418edf2400c691f01c722f1cd54061cc37b4f250
xalvaine <sopeginaliza@gmail.com>
2021-11-26 23:41:35 +0300

    [feat] Add card component

---
+++ components/Card/Card.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Card.module.css';
+
+interface Props {
+  imageSrc: string;
+  title: string;
+  subTitle: string;
+  extra?: string;
+}
+
+export const Card: React.FC<Props> = ({ imageSrc, title, subTitle, extra }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.imageWrapper}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img alt="" className={styles.image} src={imageSrc} />
+      </div>
+      <h3 className={styles.title}>{title}</h3>
+      <h4 className={styles.subTitle}>{subTitle}</h4>
+      <p className={styles.extra}>{extra}</p>
+    </div>
+  );
+};
